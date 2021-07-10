c561c5981bcdc5e1423edc6ef0cffdac007b05a9
takeru.hashimoto <hstake25@gmail.com>
2021-07-10 13:14:06 +0900

    投稿 post edit and test

--- spec/requests/v1/posts/create_spec.rb
+++ spec/requests/v1/posts/create_spec.rb
@@ -1,18 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe "V1::Posts", type: :request do
+RSpec.describe "V1::Posts::Create", type: :request do
   describe "POST /" do
-    context "未ログイン" do
+    context "ログイン時" do
       let!(:user) { create(:user) }
       let!(:category) { create(:category) }
-      let!(:posts) { create_list :post, 1, user: user, category: category }
+      let(:auth_tokens) { sign_in({ email: user.email, password: "password" }) }
 
       it "投稿できる" do
-        post "/v1/posts"
+        post "/v1/posts", headers: auth_tokens, params: {
+          post: {
+            title: "TITLE",
+            sub_title: "SUB_TITLE",
+            body: "BODY",
+            category_id: category.id
+          }
+        }
         json = JSON.parse(response.body)
         
         expect(response).to have_http_status(:success)
-        expect(json['posts'].length).to eq(1)
+        expect(json["post"]["title"]).to eq('TITLE')
+        expect(json["post"]["sub_title"]).to eq('SUB_TITLE')
+        expect(json["post"]["body"]).to eq('BODY')
+        expect(json["post"]["category_id"]).to eq(category.id)
       end
     end
   end
