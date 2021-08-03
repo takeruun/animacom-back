4b225fbf9c5845398ea71800e42264359a17ec07
takeru.hashimoto <hstake25@gmail.com>
2021-08-04 00:46:17 +0900

    カテゴリ取得テスト

--- spec/requests/v1/categories/root/index_spec.rb
+++ spec/requests/v1/categories/root/index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "V1::Categories::Index", type: :request do
+RSpec.describe "V1::Categories::Root::Index", type: :request do
   describe "GET /" do
     let!(:user) { create(:user) }
     let!(:categories) { create_list(:category, 3) }
@@ -11,43 +11,43 @@ RSpec.describe "V1::Categories::Index", type: :request do
       end
     end
     
-    shared_examples '全カテゴリが取得できる' do
+    shared_examples '第1階層のカテゴリ取得できる' do
       it do
         if defined?(auth_tokens)
-          get '/v1/categories', headers: auth_tokens
+          get '/v1/categories/root', headers: auth_tokens
         else
-          get '/v1/categories'
+          get '/v1/categories/root'
         end
         json = JSON.parse(response.body)
         
         expect(response).to have_http_status(:success)
-        expect(json['categories'].length).to eq(Category.count)
+        expect(json['root_categories'].length).to eq(categories.count)
       end
     end
 
     shared_examples '正しいカテゴリが取得できる' do
       it do
         if defined?(auth_tokens)
-          get '/v1/categories', headers: auth_tokens
+          get '/v1/categories/root', headers: auth_tokens
         else
-          get '/v1/categories'
+          get '/v1/categories/root'
         end
         json = JSON.parse(response.body)
         
         expect(response).to have_http_status(:success)
-        expect(json['categories'][0]['id']).to eq(categories.first.id.to_s)
+        expect(json['root_categories'][0]['id']).to eq(categories.first.id.to_s)
       end
     end
 
     context 'ログイン時' do
       let(:auth_tokens) { sign_in({ email: user.email, password: "password" }) }
 
-      it_behaves_like '全カテゴリが取得できる'
+      it_behaves_like '第1階層のカテゴリ取得できる'
       it_behaves_like '正しいカテゴリが取得できる'
     end
 
     context 'ログインなし' do
-      it_behaves_like '全カテゴリが取得できる'
+      it_behaves_like '第1階層のカテゴリ取得できる'
       it_behaves_like '正しいカテゴリが取得できる'
     end
   end
