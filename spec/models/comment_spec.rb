0d85ff8910d9e50d6c7409bdf2c3af288342158d
takeru.hashimoto <hstake25@gmail.com>
2021-08-07 11:12:15 +0900

    comment spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { create(:user) }
+  let!(:category) { create(:category) }
+  let!(:post) { create :post, user: user, category: category }
+  let!(:comment) { build :comment, user: user, post: post }
+
+  it "登録できる" do
+    expect(comment.valid?).to eq(true)
+  end
+
+  it "ユーザなし、登録できない" do
+    comment.user = nil
+    expect(comment.valid?).to eq(false)
+  end
+
+  it "投稿なし、登録できない" do
+    comment.post = nil
+    expect(comment.valid?).to eq(false)
+  end
 end
