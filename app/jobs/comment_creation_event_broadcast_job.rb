6c7527bddad9d4dda7c3347edce6a4ce96644768
takeru.hashimoto <hstake25@gmail.com>
2021-08-08 01:40:31 +0900

    返却変数 修正

--- app/jobs/comment_creation_event_broadcast_job.rb
+++ app/jobs/comment_creation_event_broadcast_job.rb
@@ -8,9 +8,9 @@ class CommentCreationEventBroadcastJob < ApplicationJob
       .server
       .broadcast("comment_channel_#{comment.post_id}",
                   id: comment.id,
-                  user_id: comment.user_id,
-                  post_id: comment.post_id,
-                  created_at: comment.created_at,
+                  userId: comment.user_id,
+                  postId: comment.post_id,
+                  createdAt: comment.created_at,
                   body: comment.body)
   end
 end
