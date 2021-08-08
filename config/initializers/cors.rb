af2aa88b2b9ecb3f8d897905e18ab0aa2248ce23
takeru.hashimoto <hstake25@gmail.com>
2021-08-08 21:56:55 +0900

    endpoint 指定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'anima-com.jp:3000'
+    origins 'anima-com.jp:3000', 'https://take-h.tk'
 
     resource '*',
       headers: :any,
