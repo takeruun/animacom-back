4a34ed1f5778c8fcce52cadbb243553029218940
takeru.hashimoto <hstake25@gmail.com>
2021-08-20 16:47:42 +0900

    user作成 spec

--- app/controllers/v1/users/auth/registrations_controller.rb
+++ app/controllers/v1/users/auth/registrations_controller.rb
@@ -9,4 +9,11 @@ class V1::Users::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsC
   def sign_up_params
     params.permit(:name, :email, :nickname, :image, :password, :password_confirmation)
   end
+
+  def render_create_success
+    resource_data = resource_data(resource_json: @resource.token_validation_response)
+    user_id = resource_data["id"]
+    @user = User.find(user_id)
+    render 'v1/users/show', formats: :json
+  end
 end
---
+++ spec/requests/v1/users/auth/registrations_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "V1::Users::Auth::Registrations", type: :request do
+  describe 'POST /' do
+
+    context '成功系' do
+      it 'ユーザ作成できる' do
+        post '/v1/users/auth/',
+        params: {
+          email: 'test@mail.com',
+          name: 'TEST',
+          nickname: 'NICKNAME_TEST',
+          password: '12345678',
+          image: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.png')),
+        }
+        json = JSON.parse(response.body)
+
+        expect(response).to have_http_status(:success)
+        expect(json['user']['name']).to eq('TEST')
+        expect(json['user']['nickname']).to eq('NICKNAME_TEST')
+        expect(json['user']['image_path'].present?).to eq(true)
+      end
+    end
+  end
+end
